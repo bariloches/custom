8b772586168561ca8cf1067c17b377421ebe459b
bariloches <itsmajorie@gmail.com>
2016-09-30 19:29:50 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -46,7 +46,7 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-      botName: "WoP, A Botzinha :nail_care:",
+      botName: "Flop, a Bot! :nail_care: :sparkles:",
       language: "portuguese",
       chatLink: "https://rawgit.com/carlosney/basicBot/master/lang/pt-BR.json",
       scriptLink: "https://rawgit.com/bscBot/source/master/basicBot.js",
